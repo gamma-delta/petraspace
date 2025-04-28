f2ecc766d6c805ff3cc46171a4af5144abd80168
gamma-delta <petrathekat@gmail.com>
2025-04-28 00:25:02 -0500

    make the spaceship actually spawn with the items like i wanted

--- control.lua
+++ control.lua
@@ -1,7 +1,7 @@
-require "control/freeplay"
 
 local veh = require "__core__/lualib/event_handler"
 veh.add_libraries({
+  require("control/freeplay"),
   require("control/dust-pollution"),
   require("control/ore-inclusions"),
   require("control/rockets"),
--- control/freeplay.lua
+++ control/freeplay.lua
@@ -2,15 +2,18 @@
 -- see also __base__/script/freeplay/freeplay.lua for the API
 local function add_items()
   local ship_items = remote.call("freeplay", "get_ship_items")
-  -- may do tsomething with that
-
   -- it looks like these are distributed across all the debris
   local debris_items = remote.call("freeplay", "get_debris_items")
   -- to get you started, and because that's probably what the ship is made out of.
   -- save these for later!
-  debris_items["aluminum-plate"] = 50
+  ship_items["precision-optical-component"] = 25
+  debris_items["aluminum-plate"] = 20
+  debris_items["low-density-structure"] = 50
   remote.call("freeplay", "set_debris_items", debris_items)
+  remote.call("freeplay", "set_ship_items", ship_items)
 end
 
 -- ?????
-script.on_init(add_items)
+return {
+  on_init = add_items
+}
