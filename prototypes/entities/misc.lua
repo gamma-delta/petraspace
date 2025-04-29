3310a402ca3148462c1bf3f1d0d57a0841dcdce3
gamma-delta <petrathekat@gmail.com>
2025-04-28 22:41:16 -0500

    the greenscreen was complaining for some reason, anyways this is the version the play test is on

--- prototypes/entities/misc.lua
+++ prototypes/entities/misc.lua
@@ -7,15 +7,15 @@ data:extend{
   ),
 }
 
-local greenscreen = pglobals.copy_then(
-  data.raw["tile"]["lab-dark-1"],
-  {name="greenscreen"}
-)
-greenscreen.variants.main[1].picture = "__petraspace__/graphics/tiles/greenscreen.png"
-local bluescreen = pglobals.copy_then(
-  data.raw["tile"]["lab-dark-1"],
-  {name="bluescreen"}
-)
-bluescreen.variants.main[1].picture = "__petraspace__/graphics/tiles/bluescreen.png"
-data:extend{greenscreen, bluescreen}
+-- local greenscreen = pglobals.copy_then(
+--   data.raw["tile"]["lab-dark-1"],
+--   {name="greenscreen"}
+-- )
+-- greenscreen.variants.main[1].picture = "__petraspace__/graphics/tiles/greenscreen.png"
+-- local bluescreen = pglobals.copy_then(
+--   data.raw["tile"]["lab-dark-1"],
+--   {name="bluescreen"}
+-- )
+-- bluescreen.variants.main[1].picture = "__petraspace__/graphics/tiles/bluescreen.png"
+-- data:extend{greenscreen, bluescreen}
 
